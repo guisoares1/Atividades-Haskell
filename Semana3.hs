43c20dd0d5094d94285f4794d9d642342048c0da
Guilherme Correagit config --global user.email guilhermescorrea@hotmail.com <guilhermescorrea@hotmail.com>
2021-08-31 20:39:39 -0300

    Update Semana3.hs

--- Semana3.hs
+++ Semana3.hs
@@ -11,7 +11,6 @@ somaTresNumeros n1 n2 n3 = n1 + n2 + n3
 
 {-
 Esta função retorna a área do quadrado de lado l
-
 areaQuadrado
 - Entrada: l, Float
 - Saída: l**2, Float
@@ -20,7 +19,6 @@ areaQuadrado l = l**2
 
 {-
 Esta função retorna a área do retângulo de lado l1 e l2
-
 areaRetangulo
 - Entrada: l1 e l2, Floats
 - Saída: l1 * l2, Float
@@ -31,7 +29,6 @@ areaRetangulo l1 l2 = l1 * l2
 -- Defina as seguintes funções usando if then else
 {-
 Esta função retorna o maior de tres números
-
 maiorDeTres
 - Entrada: a b c, Integer
 - Saída: o maior dentre a b c, Integer
@@ -45,17 +42,13 @@ maiorDeTres a b c = if a >= b && a >= c then a
 
 {-
 Esta função calcula quem ganha no jogo pedra/tesoura/papel
-
 pedraTesouraPapel
 - Entrada: m1 m2 - "pedra" "tesoura" "papel"
 - Saída: O valor ganhador, ou "empate" no caso empate
-
 >>>pedraTesouraPapel "pedra" "papel"
 "papel"
-
 >>>pedraTesouraPapel "pedra" "tesoura"
 "pedra"
-
 >>>pedraTesouraPapel "papel" "tesoura"
 "tesoura"
 -}
@@ -82,7 +75,6 @@ ouLógico p1 p2 = p1 || p2
 {-
 Defina função que retorne substring de t elementos começando na posição i
 subStringDeAte "entrada1" 2 2 retorna "tr"
-
 Entrada:
     - s: string
     - i: inteiro
@@ -94,23 +86,22 @@ subStringDeAte s i t = (take t (drop i s))
 
 {-
 Defina função que retorne substring com os últimos u elementos
-
 Entrada:
     - s: string
     - u: inteiro
 -}
-últimosU s u = drop u s
+últimosU:: String -> Int -> String
+últimosU s u = drop ((length s) - u) s
 {-
 Defina função que receba duas strings e retorne a resultado da concatenação das substrings de t elementos começando na posição i
-
 Entrada
     - s1: string
     - s2: string
     - i: inteiro
     - u: inteiro
 -}
-
-subStringDeAteAppend s1 s2 i u = error "Indefinido"
+subStringDeAteAppend:: String-> String-> Int -> Int -> String
+subStringDeAteAppend s1 s2 i u = (take u (drop i s1)) ++ (take u (drop i s2))
 
 
 
@@ -121,59 +112,79 @@ Seja o tipo de dados Carta tupla em que
  - o primeiro elemento é o valor da carta (1,2,3,4,5,6,7,8,9,10,11,12,13) 
  - o segundo é o naipe ("ouro", "copas", "espada", "paus").
 -}
+
 type Carta = (Int, String)
 
 {-
 Uma função que receba uma carta retorne seu naipe.
-
 Entrada:
     - c1: carta
-
 Resultado: naipe da carta
 -}
-naipe c1 = error "Indefinido"
+naipe :: Carta -> String
+naipe (_,naipe) = naipe
 
 {-
 Uma função que receba uma carta retorne seu valor.
-
 Entrada:
     - c1: carta
-
 Resultado: valor da carta
 -}
-valor c1 = error "Indefinido"
+valor :: Carta -> Int
+valor (x,_) = x
 
 
 {-
 Uma função que receba duas cartas e diga se a primeira é menor que a segunda.
 Uma carta c1 é menor que uma carta c2 se valor c1 < valor2 OU se valor c1 == valor c2 e naipe c1 < c2.
 "copas" < "espada" < "ouro" < "paus"
-
 Entrada:
     - c1, c2: Carta
 Resultado: True ou False
 -}
+menornaipe:: Carta -> Carta -> Bool
+menornaipe c1 c2 = if naipe c1=="copas" && naipe c2=="copas" then False
+                   else if naipe c1=="espada" && naipe c2=="espada" then False
+                   else if naipe c1=="ouro" && naipe c2=="ouro" then False
+                   else if naipe c1=="paus" && naipe c2=="paus" then False
+                   else if naipe c1=="copas" then True
+                   else if naipe c1 =="espada"&& naipe c2=="copas" then True
+                   else if naipe c1=="ouro"&& naipe c2=="paus" then False
+                   else True
 
-cartaÉMenor c1 c2 = error "Indefinido"
+
+cartaÉMenor:: Carta -> Carta -> Bool
+cartaÉMenor carta1 carta2 = ((valor carta1 <valor carta2 ) || (valor carta1==valor carta2 && (menornaipe carta1 carta2)) ) 
+
 
 {-
 Uma função que receba duas cartas e diga se a primeira é igual à segunda.
-
 Entrada:
     - c1, c2: Carta
 Resultado: True ou False
 -}
-
-cartaÉIgual c1 c2 = error "Indefinido"
+cartaÉIgual:: Carta -> Carta -> Bool
+cartaÉIgual carta1 carta2 = naipe carta1 == naipe carta2 && valor carta1 == valor carta2
 
 {-
 Uma função que receba duas cartas e diga se a primeira é maior que a segunda.
-
 Entrada:
     - c1, c2: Carta
 Resultado: True ou False
 -}
-cartaÉMaior c1 c2 = error "Indefinido"
+maiornaipe:: Carta -> Carta -> Bool
+maiornaipe c1 c2 = if naipe c1=="copas" && naipe c2=="copas" then False
+                   else if naipe c1=="espada" && naipe c2=="espada" then False
+                   else if naipe c1=="ouro" && naipe c2=="ouro" then False
+                   else if naipe c1=="paus" && naipe c2=="paus" then False
+                   else if naipe c1=="paus" then True
+                   else if naipe c1 =="ouro"&& naipe c2/="pau" then True
+                   else if naipe c1=="espada"&& naipe c2=="copas" then True
+                   else False
+
+
+cartaÉMaior:: Carta -> Carta -> Bool
+cartaÉMaior carta1 carta2 = ((valor carta1 >valor carta2 ) || (valor carta1==valor carta2 && (maiornaipe carta1 carta2)) )
 
 
 {-
@@ -184,13 +195,22 @@ Uma função que receba três cartas c1 c2 c3 e diga se formam um jogo.
 m1 m2 e m3 formam um jogo se e somente si
     - OU naipe m1 == naipe m2 == naipe m3 E valor m1 == valor m2 + 1 == valor m3 + 2
     - OU naipe m1 =/= naipe m2 =/= naipe m3 =/= m1 E valor m1 == valor m2 == valor m3
-
-
 Entrada:
     - c1, c2, c3: Carta
 Resultado: True ou False
 -}
-éJogo c1 c2 c3 = error "Indefinido"
+fazjogo :: Carta -> Carta -> Carta -> Bool
+
+
+fazjogo m1 m2 m3 = (((naipe m1 == naipe m2 && naipe m2 == naipe m3) && ((valor m1) == ((valor m2) + 1) && ((valor m2) + 1) == ((valor m3) + 2))) ||  ((naipe m1 /= naipe m2 && naipe m2  /= naipe m3 && naipe m3/= naipe m1) && (valor m1 == valor m2 && valor m2 == valor m3)))
+
+éJogo:: Carta -> Carta -> Carta -> Bool
+éJogo c1 c2 c3 = if cartaÉMaior c1 c2 &&  cartaÉMaior c2 c3 then fazjogo c1 c2 c3
+                 else if cartaÉMaior c1 c3 &&  cartaÉMaior c3 c2 then fazjogo c1 c3 c2
+                 else if cartaÉMaior c2 c1 &&  cartaÉMaior c1 c3 then fazjogo c2 c1 c3
+                 else if cartaÉMaior c2 c3 &&  cartaÉMaior c3 c1 then fazjogo c2 c3 c1
+                 else if cartaÉMaior c3 c1 &&  cartaÉMaior c1 c2 then fazjogo c3 c1 c2
+                 else fazjogo c3 c2 c1
 
 {-
 Defina uma função que receba duas tuplas de 3 cartas, onde a primeira carta é maior ou igual à segunda, que é maior ou igual à terceira, 
@@ -200,7 +220,21 @@ Uma tupla t1 é menor que uma tupla t2 se e somente se
 - as primeiras cartas são iguais mas a segunda carta de t1 é menor que a segunda de t2
 - as primeiras e segundas cartas são iguais mas a terceira carta de t1 é menor que a terceira de t2
 -}
-maiorMão t1 t2 = error "Indefinido"
+type Cartasmao = (Carta, Carta, Carta)
+primeiraCarta:: Cartasmao-> Carta 
+primeiraCarta (x,_,_) = x
+segundaCarta :: Cartasmao-> Carta
+segundaCarta (_,x,_) = x
+terceiraCarta :: Cartasmao-> Carta 
+terceiraCarta  (_,_,x) = x 
+
+
+
+maiorMão :: Cartasmao -> Cartasmao -> Cartasmao
+maiorMão mao1 mao2 = if (cartaÉMenor (primeiraCarta mao1)  (primeiraCarta mao2)) then mao2
+                     else if (cartaÉIgual (primeiraCarta mao1)  (primeiraCarta mao2))  && (cartaÉMenor (segundaCarta mao1) (segundaCarta mao2))  then mao2
+                     else if (cartaÉIgual (primeiraCarta mao1)) (primeiraCarta mao2) && (cartaÉIgual (segundaCarta mao1) (segundaCarta mao2)) && (cartaÉMenor (terceiraCarta mao1)  (terceiraCarta mao2)) then mao2
+                     else mao1
 
 {-
 Defina uma função que receba duas tuplas de 3 cartas, onde a primeira carta é maior ou igual à segunda, que é maior ou igual à terceira, 
